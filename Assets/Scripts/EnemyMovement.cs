8f7d82e08af2a3b9bea8dca25a30be5b7258d64b
zhannigo <drobanovazh@gmail.com>
2021-08-11 08:24:10 +0300

    Added animation and old player script (PlayerMovement)

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private SpriteRenderer render_Enemy;
+    Animator e_Animator;
+    public float _speed;
+    public Rigidbody2D rb_Enemy;
+    private Vector2 _movement;
+    public int _maxHP;
+    private int _currentHP;
+    private bool _isDie=false;
+
+    public Transform targetGameObject;
+    public float targetDistance = -1;
+    public LayerMask targetLayer;
+
+    public int e_attackDamage;
+    private bool e_isAttack = false;
+
+    private void Start()
+    {
+        render_Enemy = GetComponent<SpriteRenderer>();
+        e_Animator = GetComponent<Animator>();
+        rb_Enemy = GetComponent<Rigidbody2D>();
+    }
+    void Awake()
+    {
+        _movement = new Vector2(_speed, 0);
+        _currentHP = _maxHP;
+    }
+    private void FixedUpdate()
+    {
+        if (!_isDie)
+        {
+            if (!e_isAttack)
+            {
+                Move();
+            }
+
+            Vector2 direction = targetGameObject.position;
+
+            /*RaycastHit2D hit = Physics2D.Raycast(rb_Enemy.position, direction, targetDistance, targetLayer);
+            if (hit.collider != null)
+            {
+                //Debug.Log("Attack is" + hit.collider.name);
+            }*/
+            RaycastHit2D hit_back = Physics2D.Raycast(rb_Enemy.position, direction, -targetDistance, targetLayer);
+
+            if(hit_back.collider != null)
+            {
+                Flip();
+            }
+
+            Debug.DrawRay(rb_Enemy.position, direction, Color.red);
+        }
+    }
+        private void Move()
+    {
+        rb_Enemy.AddForce(_movement);
+        e_Animator.SetBool("IsWalking",true);
+    }
+    public void Flip()
+    {
+        rb_Enemy.velocity = Vector2.zero;
+        /*Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;*/
+        _movement = -_movement;
+        render_Enemy.flipX = ! render_Enemy.flipX;
+        targetDistance = -targetDistance;
+    }
+    public void EnemyAttack()
+    {
+            rb_Enemy.velocity = Vector2.zero;
+            e_Animator.SetBool("IsWalking", false);
+            e_Animator.SetTrigger("Attack");
+    }
+    public void TakeDamage(int damage)
+    {
+        e_Animator.SetTrigger("Hurt");
+        _currentHP -= damage;
+
+        if (_currentHP <= 0 && !_isDie)
+        {
+            rb_Enemy.velocity = Vector2.zero;
+            e_Animator.SetTrigger("Dead");
+            _isDie = true;
+            StartCoroutine("Die");
+        }
+    }
+    /*private void Die()
+    {
+        
+    }*/
+    IEnumerator Die()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        this.enabled = false;
+        //etComponent<Collider2D>().enabled = false;
+        Destroy(gameObject,1.1f);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,198 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Joystick joystick;
+    protected JoyButton joyButton;
+
+    private Rigidbody2D m_Rigidbody2D;
+    public Animator m_Animator;
+
+    public HealthBar healthBar;
+
+    public Transform attackPoint;
+    public float _attackRange;
+    public int _attackDamage;
+    public LayerMask enemyLayer;
+    private readonly float attackRate = 2f;
+    private float nextAttackTime = 0f;
+    
+    public float runSpeed = 60f;
+    private float horizontalMove;
+    private bool m_FacingRight = true;
+    public float m_JumpForce;
+    private bool _jump = false;
+    private Vector2 m_Velocity = Vector2.zero;
+    private readonly float m_MovementSmoothing = .1f;
+
+    //находится ли персонаж на земле или в прыжке?
+    private bool isGrounded;
+    //ссылка на компонент Transform объекта для определения соприкосновения с землей
+    public Transform groundCheck;
+    //ссылка на слой, представляющий землю
+    public LayerMask whatIsGround;
+    //радиус коллайдера
+    public float groundRadius;
+
+    public int _maxPlayerHP;
+
+
+    public int _currentPlayerHP;
+
+    public int CurrentPlayerHP
     {
-        
+        get 
+        { 
+            return _currentPlayerHP; 
+        }
+
+        set
+        {
+            Debug.Log($"{value}, {_currentPlayerHP}");
+            _currentPlayerHP = _currentPlayerHP >= 0 ? value : 0;
+        }
     }
 
-    // Update is called once per frame
+    bool _isDie = false;
+
+    private void Awake()
+    {
+        //joystick = FindObjectOfType<Joystick>();
+        //joyButton = FindObjectOfType<JoyButton>();
+
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        m_Animator = GetComponent<Animator>();
+        healthBar.SetMaxHealth(_maxPlayerHP);
+        CurrentPlayerHP = _maxPlayerHP;
+    }
     void Update()
     {
-        
+       horizontalMove = Input.GetAxis("Horizontal") * runSpeed;
+
+        if(Time.time >= nextAttackTime)
+        {
+            if (Input.GetButtonDown("Hit"))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+                _jump = true;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        // определяем, на земле ли персонаж
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        m_Animator.SetBool("isJumping", !isGrounded);
+
+        if (_jump == true)
+        {
+            Jump();
+        }
+        //Move(joystick.Horizontal + horizontalMove);
+        Move(horizontalMove);
+
+        /*if (!isGrounded)
+           {
+               return;
+           }*/
+    }
+    public void Move(float move)
+    {
+
+            //m_Rigidbody2D.AddForce(new Vector2(move, 0), ForceMode2D.Impulse);
+            //m_Rigidbody2D.AddForce(new Vector2(move, m_Rigidbody2D.velocity.y), ForceMode2D.Impulse);
+            Vector2 targetVelocity = new Vector2(move * 10, m_Rigidbody2D.velocity.y);
+            m_Rigidbody2D.velocity = Vector2.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+            //m_Animator.SetFloat("Speed", Mathf.Abs(horizontalMove+ joystick.Horizontal));
+            m_Animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+
+        if (move > 0 && !m_FacingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && m_FacingRight)
+        {
+            Flip();
+        }
+    }
+    private void Jump()
+    {
+            //m_Rigidbody2D.AddForce(new Vector2(horizontalMove, m_JumpForce), ForceMode2D.Impulse);
+            m_Rigidbody2D.AddForce(Vector2.up * m_JumpForce, ForceMode2D.Impulse);
+            //m_Rigidbody2D.AddForce(new Vector2(0, verticalMove * m_JumpForce), ForceMode2D.Impulse);
+            _jump = false;
+    }
+    public void Flip()
+    {
+        m_FacingRight = !m_FacingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+    private void Attack()
+    {
+        m_Animator.SetTrigger("Attack");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, _attackRange, enemyLayer);
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            //Debug.Log("We hit"+ enemy.name);
+            enemy.GetComponent<EnemyMovement>().TakeDamage(_attackDamage);
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, _attackRange);
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+           int enemy_damage = other.gameObject.GetComponent<EnemyMovement>().e_attackDamage;
+            other.gameObject.GetComponent<EnemyMovement>().EnemyAttack();
+            TakeDamage(enemy_damage);
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        if(CurrentPlayerHP > 0)
+        {
+            CurrentPlayerHP = CurrentPlayerHP - damage;
+            m_Animator.SetTrigger("Damage");
+            healthBar.SetHealth(CurrentPlayerHP);
+            Debug.Log("Damage" + damage);
+        }
+
+        if (CurrentPlayerHP <= 0 && !_isDie)
+        {
+            _isDie = true;
+            StartCoroutine("Die");
+        }
+    }
+    IEnumerator Die()
+    {
+        yield return new WaitForSeconds(1.5f);
+        m_Animator.SetTrigger("Dead");
+        Debug.Log("гг умирает");
+        FinMenu();
+    }
+    public void FinMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 }
